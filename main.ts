0ccd4b6018af209727a419f1d4039aa3552af8f0
craig-dsilva <craig.s.dsilva@gmail.com>
2022-09-23 21:14:37 +0100

    movement according to input

--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
 import prompt from 'prompt';
 
-prompt.start();
+prompt.start({ noHandleSIGINT: false });
+
+process.on('SIGINT', function () {
+    console.log("This will execute when you hit CTRL+C");
+    process.exit();
+});
 
 const promptsAllowed = ['u', 'd', 'l', 'r', 'p', 'e'];
 
 const promptSchema = {
     properties: {
         input: {
+            description: 'Provide an input',
             enum: promptsAllowed,
             message: 'Unknown input. Try again.',
             required: true
@@ -22,5 +28,20 @@ arr[POS.y][POS.x] = 'C';
 console.log(arr);
 
 prompt.get(promptSchema, (err, result) => {
-    console.log(result.input);
+    arr[POS.y][POS.x] = '';
+    const direction = result.input;
+    if (direction === 'u') {
+        POS.y -= 1;
+    }
+    if (direction === 'd') {
+        POS.y += 1;
+    }
+    if (direction === 'l') {
+        POS.x -= 1;
+    }
+    if (direction === 'r') {
+        POS.x += 1;
+    }
+    arr[POS.y][POS.x] = 'C';
+    console.log(arr);
 });
