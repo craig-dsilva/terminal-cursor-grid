2516843239f70b9f00ea4139b0a5656d09042c44
craig-dsilva <craig.s.dsilva@gmail.com>
2022-09-23 20:48:33 +0100

    prompt with validator

--- main.ts
+++ main.ts
@@ -2,6 +2,25 @@ import prompt from 'prompt';
 
 prompt.start();
 
-prompt.get(['input'], (err, result) => {
+const promptsAllowed = ['u', 'd', 'l', 'r', 'p', 'e'];
+
+const promptSchema = {
+    properties: {
+        input: {
+            enum: promptsAllowed,
+            message: 'Unknown input. Try again.',
+            required: true
+        }
+    }
+}
+
+const arr = [['', '', ''], ['', '', ''], ['', '', '']];
+const POS = { x: 0, y: 0 };
+
+arr[POS.y][POS.x] = 'C';
+
+console.log(arr);
+
+prompt.get(promptSchema, (err, result) => {
     console.log(result.input);
 });
